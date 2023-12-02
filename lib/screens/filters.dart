59af3bff62ceb86ff74d3988289f5965eb756a68
hoosamMr <hoosam.mr@gmail.com>
2023-12-02 22:50:15 +0200

    continue the drawer, adding filtersScreen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget{
+  const FiltersScreen({super.key});
+  
+  @override
+  State<FiltersScreen> createState() {
+   return _FiltersScreen();
+  }
+
+
+}
+class _FiltersScreen extends State<FiltersScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
