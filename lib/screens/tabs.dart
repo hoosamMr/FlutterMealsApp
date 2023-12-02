227b152e5e9999aa26284433837900af8d7f4309
hoosamMr <hoosam.mr@gmail.com>
2023-12-02 22:48:54 +0200

    continue the drawer, adding filtersScreen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -71,7 +71,13 @@ class _TabsScreen extends State<TabsScreen> {
       // }
     });
   }
+  void _setScreen(String identifier){
+    if(identifier=='filters'){
 
+    }else{
+      Navigator.of(context).pop();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     Widget activeScreen = CategoriesScreen(
@@ -90,7 +96,7 @@ class _TabsScreen extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-        drawer: const MainDrawer(),
+        drawer:  MainDrawer(onSelectScreen: _setScreen,),
       body: activeScreen,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectedPage,
